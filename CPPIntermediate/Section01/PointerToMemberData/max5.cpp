c40ef4c627768937e1bf0a0830df1a08716e909c
JuYoung Kim <robolab1902@naver.com>
2022-11-30 15:04:22 +0900

    멤버 변수 포인터 쓸 수 있는 방법 설명하며 max 구현

---
+++ CPPIntermediate/Section01/PointerToMemberData/max5.cpp
@@ -0,0 +1,31 @@
+/*#include <iostream>
+#include <string>
+#include <functional>
+
+template<class T, class Proj = std::identity>
+const T& mymax(const T& obj1, const T& obj2, Proj proj = {})
+{
+	return std::invoke(proj, obj1) < std::invoke(proj, obj2) ? obj2 : obj1;
+}
+
+struct Point
+{
+	int x, y;
+};
+
+int main()
+{
+	std::string s1 = "abcd";
+	std::string s2 = "xyz";
+
+	auto ret1 = mymax(s1, s2);
+	auto ret2 = mymax(s1, s2, [](auto& a) {return a.size(); });
+	auto ret3 = mymax(s1, s2, &std::string::size);
+
+	Point p1 = { 2,0 };
+	Point p2 = { 1,1 };
+
+	auto ret4 = mymax(p1, p2, &Point::y);
+
+	std::cout << ret4.x << ", " << ret4.y << std::endl;
+}*/
