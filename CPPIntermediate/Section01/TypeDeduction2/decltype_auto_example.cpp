0b92b0034b8c88429f6e44b005b2ea05c5636783
JuYoung Kim <robolab1902@naver.com>
2022-12-08 17:48:43 +0900

    Type Deduction 에서 decltype 확인

---
+++ CPPIntermediate/Section01/TypeDeduction2/decltype_auto_example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template<typename T1, typename T2> 
+// auto Add(T1 a, T2 b) -> decltype(a+b)
+// auto Add(T1 a, T2 b)
+decltype(auto) Add(T1 a, T2 b)
+{
+	return a + b;
+}
+
+int main()
+{
+	std::cout << Add(1, 2) << std::endl;
+	std::cout << Add(1, 2.3) << std::endl;
+}
