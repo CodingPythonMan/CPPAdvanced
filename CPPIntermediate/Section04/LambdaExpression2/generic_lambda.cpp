8af5481a94829883df22102d1c68fd737ba612e7
JuYoung Kim <robolab1902@naver.com>
2022-12-09 16:38:20 +0900

    Lambda 는 C++11 부터 추가되었지만 auto, template 부터는 20부터 추가되었다

---
+++ CPPIntermediate/Section04/LambdaExpression2/generic_lambda.cpp
@@ -0,0 +1,10 @@
+/*#include <iostream>
+
+int main()
+{
+	auto add = [](auto a, auto b) {return a + b; };
+
+	std::cout << add(1, 1) << std::endl;
+	std::cout << add(1.1, 1.2) << std::endl;
+	std::cout << add(1, 1.4) << std::endl;
+}*/
