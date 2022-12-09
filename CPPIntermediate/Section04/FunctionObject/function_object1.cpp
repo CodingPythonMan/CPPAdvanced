ec20aaee064ec63400823fb4284e9e4004ace026
JuYoung Kim <robolab1902@naver.com>
2022-12-09 16:20:21 +0900

    함수 객체에 대해서 설명하고, 함수 객체가 왜 상태를 저장할 수 있는지 설명함. 람다에 대해 설명하며 람다가 함수객체를 만드는 것이라는 것과 내부에 inline 속성을 가지고 있다고 설명함

---
+++ CPPIntermediate/Section04/FunctionObject/function_object1.cpp
@@ -0,0 +1,19 @@
+/*#include <iostream>
+
+struct plus
+{
+	int operator()(int arg1, int arg2)
+	{
+		return arg1 + arg2;
+	}
+};
+
+int main()
+{
+	plus p;							// p 는 객체
+
+	int n = p(1, 2);				// p 를 함수처럼 사용.
+									// p.operator()(1,2)
+
+	std::cout << n << std::endl;
+}*/
