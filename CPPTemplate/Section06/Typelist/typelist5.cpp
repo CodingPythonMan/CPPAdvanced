df84d6462a11ea6d2e23974eaed023ff15d4553b
JuYoung Kim <robolab1902@naver.com>
2022-12-13 17:27:46 +0900

    Typelist 에 대한 소개

---
+++ CPPTemplate/Section06/Typelist/typelist5.cpp
@@ -0,0 +1,51 @@
+/*#include <iostream>
+using namespace std;
+
+template<typename T, typename U> struct Typelist
+{
+	typedef T Head;
+	typedef U Tail;
+};
+
+struct NullType {};
+
+#define TYPELIST_1(T1)		Typelist<T1, NullType>
+#define TYPELIST_2(T1, T2)	Typelist<T1, Typelist<T2, NullType>>
+#define TYPELIST_3(T1, T2, T3)	Typelist<T1, Typelist<T2, Typelist<T3, NullType>>>
+#define TYPELIST_4(T1, T2, T3, T4)	Typelist<T1, Typelist<T2, Typelist<T3, Typelist<T4, NullType>>>>
+
+// Typelist 의 N번째 요소의 타입 구하기
+// 1. 사용하는 코드를 보고 primary template 작성.
+// T : Typelist
+template<typename T, int N> struct TypeAt;
+
+// 2. 원하는 타입을 구할 수 있도록 부분 특수화
+// T : Typelist 의 요소 타입
+//template<typename T, typename U, int N> struct TypeAt<Typelist<T, U>, N>
+//{
+//	typedef ? type;
+//};
+
+// N == 0 일 때
+template<typename T, typename U> struct TypeAt<Typelist<T, U>, 0>
+{
+	typedef T type;
+};
+
+// N != 0 일 때
+template<typename T, typename U, int N> struct TypeAt<Typelist<T, U>, N>
+{
+	typedef typename TypeAt<U, N-1>::type type;
+};
+
+template<typename T> void test()
+{
+	typename TypeAt<T, 1>::type n;	// char
+
+	cout << typeid(n).name() << endl;
+};
+
+int main()
+{
+	test<TYPELIST_4(int, char, double, long)>();
+}*/
---
+++ CPPTemplate/Section06/Typelist/typelist8.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+template<typename T, typename U> struct Typelist
+{
+	typedef T Head;
+	typedef U Tail;
+};
+
+struct NullType {};
+
+#define TYPELIST_1(T1)		Typelist<T1, NullType>
+#define TYPELIST_2(T1, T2)	Typelist<T1, Typelist<T2, NullType>>
+#define TYPELIST_3(T1, T2, T3)	Typelist<T1, Typelist<T2, Typelist<T3, NullType>>>
+#define TYPELIST_4(T1, T2, T3, T4)	Typelist<T1, Typelist<T2, Typelist<T3, Typelist<T4, NullType>>>>
+
+// Typelist 활용
+// Holder : 임의 타입의 data 하나 보관..
+template<typename T> struct Holder
+{
+	T value;
+};
+
+// GenScatterHierachy => MakeCode
+template<typename T, template<typename> class Unit>
+class MakeCode : public Unit<T>
+{
+
+};
+
+template<template<typename> class Unit>
+class MakeCode<NullType, Unit>
+{
+
+};
+
+template<typename Head,						//							Holder<double>, empty
+		typename Tail,						// Holder<int>				MakeCode<double, Unit>, MakeCode<NullType, Unit>
+		template<typename> class Unit>		// MakeCode<int, Unit>,		MakeCode<Typelist<double, NullType>, Unit>
+class MakeCode<Typelist<Head, Tail>, Unit> : public MakeCode<Head,Unit>, public MakeCode<Tail, Unit>
+{
+	// int value
+	// double value
+};
+
+int main()
+{
+	//Holder<TYPELIST_3(int, double, short), Holder> m1;
+	// MakeCode 의 1번째 인자가 Typelist 일 때..
+	// Typelist<int, Typelist<double, NullType>
+	//Holder<TYPELIST_2(int, double), Holder> m2;
+
+	MakeCode<int, Holder> mc1;		// Holder<int>
+	MakeCode<double, Holder> mc2;	// Holder<double>
+	MakeCode<NullType, Holder> mc3; // empty
+}
