5a9f7fbb3c3adbb9c8ecd456d7540d68ed8cdab2
JuYoung Kim <robolab1902@naver.com>
2022-12-22 11:39:21 +0900

    advance 가 C++이 발전해오면서 어떻게 구현법들이 변화해왔는지 확인할 수 있었다.

---
+++ CPPAdvanced/Section03/Advance/advance1.cpp
@@ -0,0 +1,19 @@
+/*#include <iostream>
+#include <vector>
+#include <list>
+#include <iterator>
+
+int main()
+{
+	std::vector<int> c = { 1,2,3,4,5,6,7,8,9,10 };
+	//std::list<int> c = { 1,2,3,4,5,6,7,8,9,10 };
+
+	auto p = std::begin(c);
+
+	// p = p+5;
+	//++p; ++p; ++p; ++p; ++p;
+
+	std::advance(p, 5);
+
+	std::cout << *p << std::endl;
+}*/
